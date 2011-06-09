81b0138448aa956b3b115c8915d874b843183e20
Fletcher Nichol <fnichol@nichol.ca>
2011-06-08 21:38:34 -0600

    Fix readme link in metadata.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures XBMC"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 
 %w{ ubuntu }.each do |os|
